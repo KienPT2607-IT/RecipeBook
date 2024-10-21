95eb9462bae1291b1e8f09a5152bf32b4edd5775
KienPT.IT <phamkien.8a@gmail.com>
2024-10-21 16:45:02 +0700

    add top bar

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+  styleUrls: ['./top-bar.component.scss'],
 })
 export class TopBarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  logo: string = '../../../assets/icons/vietnam-flag-icon.svg';
+  caretDownIcon: string =
+    '.././../../assets/icons/caret-down-thin-svgrepo-com.svg';
 }
